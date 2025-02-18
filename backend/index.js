dcd371628bdfa1bc1e9dc4227d2ab5743e00bb0f
mekonnen <mekonnen.cs@gmail.com>
2025-02-18 01:40:46 -0500

    Added the frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
-import express form "express";
+import express from "express";
 import cors from "cors";
+import userRoutes from "./routes/users.js";
 
 const app = express();
-app.use(express.json())
-app.use(cors())
-app.listen(8800)
+app.use(express.json());
+app.use(cors());
+app.use("/", userRoutes);
+app.listen(8800);
