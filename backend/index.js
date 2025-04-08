70da7883dd52e7392a010f2c7d6931590dc3a49b
mekonnen <mekonnen.cs@gmail.com>
2025-04-08 02:59:59 -0400

    added apprunner

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,6 @@ import userRoutes from "./routes/users.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
-// app.use("/", userRoutes);
-app.use("/api/users", userRoutes);
+app.use("/", userRoutes);
+// app.use("/api/users", userRoutes);
 app.listen(8800);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ function App() {
     try {
       // const res = await axios.get("http://localhost:8800");
       const res = await axios.get(
-        "https://vqvfhiwwpr.us-east-2.awsapprunner.com/api/users"
+        "https://vqvfhiwwpr.us-east-2.awsapprunner.com"
       );
       setUsers(res.data.sort((a, b) => (a.name > b.name ? 1 : -1)));
     } catch (error) {
