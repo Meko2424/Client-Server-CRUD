c0ac9b4964ecdae6d1d8570fb995957786f744b4
mekonnen <mekonnen.cs@gmail.com>
2025-04-07 22:49:35 -0400

    Host changed

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 export const db = mysql.createConnection({
   // host: "localhost",
-  host: "webservice.cwls0k0o8hni.us-east-1.rds.amazonaws.com",
+  host: "webservice.cz2a2og0qa52.us-east-2.rds.amazonaws.com",
   user: "admin",
   password: "minimumOf12*",
   port: "8800",
