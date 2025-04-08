d261bae299413d13fd9dfaab635c5eaf9bb08cad
mekonnen <mekonnen.cs@gmail.com>
2025-04-07 23:23:12 -0400

    Modified URL's

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,7 +9,7 @@ import {
 const router = express.Router();
 
 router.get("/", getUsers);
-router.post("/", addUser);
+router.post("/users", addUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
