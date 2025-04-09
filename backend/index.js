bbdc85901a04e28960960bd51d282113deef1757
mekonnen <mekonnen.cs@gmail.com>
2025-04-09 19:39:31 -0400

    url

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,10 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/users.js";
 
-const PORT = process.env.PORT || 8800;
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/", userRoutes);
 // app.use("/api/users", userRoutes);
-// app.listen(8800);
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-
-  app.get("/ping", (req, res) => {
-    res.send("pong");
-  });
-});
+app.listen(8800);
+app.listen(PORT, () => {});
