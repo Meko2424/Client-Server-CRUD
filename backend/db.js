cbdd1b6398c2cfc7604cd50cede7dc27e4b3a9cb
mekonnen <mekonnen.cs@gmail.com>
2025-04-10 19:29:16 -0400

    db url modified

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  // host: "webservice.cz2a2og0qa52.us-east-2.rds.amazonaws.com",
-  host: "https://vqvfhiwwpr.us-east-2.awsapprunner.com",
+  host: "webservice.cz2a2og0qa52.us-east-2.rds.amazonaws.com",
+  // host: "https://vqvfhiwwpr.us-east-2.awsapprunner.com",
   user: "admin",
   password: "minimumOf12*",
   //port: 3306,
