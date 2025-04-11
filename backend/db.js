42caa7fd03f1129a41d7e06585f323124510e9a5
mekonnen <mekonnen.cs@gmail.com>
2025-04-10 22:22:51 -0400

    Db modified

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,30 @@
 import mysql from "mysql";
 
-export const db = mysql.createConnection({
+// export const db = mysql.createConnection({
+//   host: "webservice.cz2a2og0qa52.us-east-2.rds.amazonaws.com",
+//   // host: "https://vqvfhiwwpr.us-east-2.awsapprunner.com",
+//   user: "admin",
+//   password: "minimumOf12*",
+//   //port: 3306,
+//   database: "students",
+
+//   ///////////////////////////
+//   // host: "localhost",
+//   // user: "root",
+//   // password: "password",
+//   // database: "students",
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to MySQL: ", err);
+//     return;
+//   }
+//   console.log("Connected to MySQL database!");
+// });
+
+export const db = mysql.createPool({
+  connectionLimit: 10,
   host: "webservice.cz2a2og0qa52.us-east-2.rds.amazonaws.com",
   // host: "https://vqvfhiwwpr.us-east-2.awsapprunner.com",
   user: "admin",
@@ -13,12 +37,4 @@ export const db = mysql.createConnection({
   // user: "root",
   // password: "password",
   // database: "students",
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL: ", err);
-    return;
-  }
-  console.log("Connected to MySQL database!");
 });
