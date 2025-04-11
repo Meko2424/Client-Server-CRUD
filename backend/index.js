ffa9bb34bf85630580ef5b1b0f471560fcd0af6b
mekonnen <mekonnen.cs@gmail.com>
2025-04-10 21:06:43 -0400

    test

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,13 @@ import userRoutes from "./routes/users.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+app.get("/test", (req, res) => {
+  res.send("Backend is up!");
+});
 app.use("/", userRoutes);
 // app.use("/api/users", userRoutes);
 app.listen(8800);
