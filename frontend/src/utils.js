23bee162d869fdc524cf3317f6abcf2db3f67161
mekonnen <mekonnen.cs@gmail.com>
2025-04-13 22:59:47 -0400

    modified version

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,5 @@
 // export const API_URL =
 //   "https://jja6sbewqslqqjo35c5gvfxvmy0uvadb.lambda-url.us-east-2.on.aws/test";
 
-export const API_URL = "http://localhost:3001/test";
+export const API_URL =
+  "https://4yihkpykx2nvwqtowcjt57ziga0yhsyx.lambda-url.us-east-2.on.aws/";
