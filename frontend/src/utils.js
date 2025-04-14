c09a966a64f45f507d6967a3228b381d1fccfbf1
mekonnen <mekonnen.cs@gmail.com>
2025-04-13 21:29:54 -0400

    using localhost

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,2 +1,4 @@
-export const API_URL =
-  "https://jja6sbewqslqqjo35c5gvfxvmy0uvadb.lambda-url.us-east-2.on.aws/test";
+// export const API_URL =
+//   "https://jja6sbewqslqqjo35c5gvfxvmy0uvadb.lambda-url.us-east-2.on.aws/test";
+
+export const API_URL = "http://localhost:3001/test";
