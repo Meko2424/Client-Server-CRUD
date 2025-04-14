6c533ae58013b6bc5ed8a69e92ce2e827c3a56d1
mekonnen <mekonnen.cs@gmail.com>
2025-04-13 21:49:08 -0400

    modified api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,10 +8,9 @@ import {
 
 const router = express.Router();
 
-router.get("/");
-router.get("/test", getUsers);
-router.post("/test", addUser);
-router.put("/test:id", updateUser);
-router.delete("/test:id", deleteUser);
+router.get("/", getUsers);
+router.post("/", addUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
